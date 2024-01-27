b1642412d8c038fcf922e36f9b40c82effdc2ff2
QuyetTV1991 <quyettv.bk@gmail.com>
2024-01-27 15:14:40 +0800

    Commit Banner Component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import Banner from "@/components/shared/Banner";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center p-12 sm:px-4">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+          bannerDesc="Discover, collect and sell extraordinay NFTs"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
