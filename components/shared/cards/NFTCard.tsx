442fc5fdd80f897effab94bcbd2293e4eae495f1
QuyetTV1991 <quyettv.bk@gmail.com>
2024-01-28 09:00:49 +0800

    fix the card width on sm responsive

--- components/shared/cards/NFTCard.tsx
+++ components/shared/cards/NFTCard.tsx
@@ -16,7 +16,7 @@ interface NFTCardProps {
 const NFTCard = ({ nft }: NFTCardProps) => {
   return (
     <Link href={`/nft-details/${nft.id}`}>
-      <div className="m-4 min-w-215 max-w-max flex-1 cursor-pointer rounded-2xl bg-white p-4 shadow-md dark:bg-nft-black-3 sm:m-2 sm:w-full sm:min-w-155 xs:max-w-none minmd:min-w-256 minlg:m-8 minlg:min-w-327">
+      <div className="m-4 min-w-215 max-w-max flex-1 cursor-pointer rounded-2xl bg-white p-4 shadow-md dark:bg-nft-black-3 sm:m-2 sm:w-full sm:min-w-[255px] xs:max-w-none minmd:min-w-256 minlg:m-8 minlg:min-w-327">
         <div className="relative h-52 w-full overflow-hidden rounded-2xl sm:h-36 xs:h-56 minmd:h-60 minlg:h-300">
           <Image
             src={nft.image || "/assets/nft-default.png"}
