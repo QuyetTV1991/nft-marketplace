dbe649a0f67c508fb21bc016636befcfb50eb3a6
QuyetTV1991 <quyettv.bk@gmail.com>
2024-03-08 19:21:39 +0800

    banner

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,19 @@ import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute='class'>
+    <ThemeProvider attribute="class">
       <div className="dark:bg-nft-dark bg-white min-h-screen">
         <Navbar />
-        <Component {...pageProps} />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
         <Footer />
       </div>
 
-      <Script src="https://kit.fontawesome.com/c7d9204842.js" crossOrigin="anonymous" />
+      <Script
+        src="https://kit.fontawesome.com/c7d9204842.js"
+        crossOrigin="anonymous"
+      />
     </ThemeProvider>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-export default function Home() {
+import { Banner } from "../components";
+
+const Home = () => {
   return (
-   <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-   </div>
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+          name="Discover, collect and sell extraordinary NFTs"
+        />
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
